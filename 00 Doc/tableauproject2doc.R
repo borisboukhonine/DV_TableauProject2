2fa64576c7b2cbedb21c3ab2c9f2e717fe5a4c9a
Boris Boukhonine <boukhonine@wireless-10-145-183-160.public.utexas.edu>
2015-04-10 20:11:28 -0500

    work in prog 2

--- 00 Doc/tableauproject2doc.R
+++ 00 Doc/tableauproject2doc.R
@@ -1,15 +1,15 @@
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select 
-case when grouping(group_name)=1 then \\\'zTotal\\\' 
-else group_name end group_name, 
-case when grouping(status)=1 then \\\'yTotal\\\' else status end status, 
-count(*) n 
-from ethnic_power_relations2 group by cube (group_name, status)"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_bb25684', PASS='orcl_bb25684', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
+# Crosstab 1
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select
-GROUP_NAME, STATUS, count(*) n
-from ethnic1 group by cube (group_name, status)"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='C##cs329e_bb25684', PASS='orcl_bb25684', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
+"select * from (SELECT year, states, cigarette_tax_dollar_per_pack, (rank() 
+OVER (PARTITION BY year order by cigarette_tax_dollar_per_pack desc)) as tax_rank 
+FROM cig_tax where (year in (1995, 2000, 2010, 2013)) order by 1, 2) where tax_rank < 11"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_bb25684', PASS='orcl_bb25684', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df)
 
-spread(df, DNAME, N)
+# Crosstab 2
+
+
+
+
+
+
